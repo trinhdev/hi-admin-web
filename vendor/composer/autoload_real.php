fd893c209db5e9b563fc69ae6ea4911c8a612ea5
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2021-11-10 17:21:36 +0700

    test bug

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -26,9 +26,8 @@ class ComposerAutoloaderInit6f117cb46fd2d477c86c18729fd55bd5
         spl_autoload_register(array('ComposerAutoloaderInit6f117cb46fd2d477c86c18729fd55bd5', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit6f117cb46fd2d477c86c18729fd55bd5', 'loadClassLoader'));
-
+        die("dong 29");
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        die("dong 31");
         if ($useStaticLoader) {
             require __DIR__ . '/autoload_static.php';
 
