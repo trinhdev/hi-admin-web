6165ea3d6cf5c42d5a55de1279f775bd6ef7b91f
unknown <khoatb2@fpt.com.vn>
2021-11-22 16:17:04 +0700

    clear cache  git

--- vendor/igaster/laravel-theme/src/Exceptions/themeAlreadyExists.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Igaster\LaravelTheme\Exceptions;
-
-class themeAlreadyExists extends \Exception{
-
-	public function __construct($theme) {
-		parent::__construct("Theme {$theme->name} already exists", 1);
-	}
-
-}
