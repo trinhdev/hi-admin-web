008c7df6390b38c4c289c83b306ae41d11dccac1
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2021-11-22 21:12:27 +0700

    clear main

--- resources/views/admin/user-management/role-detail.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('app')
-
-@section('title','Role Detail')
-@section('content')
-    <role-detail :rolegroup="{{ json_encode($data['roleDetail']) }}" :users="{{ json_encode($data['users']) }}"></role-detail>
-@endsection
