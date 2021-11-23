0c99202ac07e9ad42bfd5bb479fa771ab910d81c
unknown <oanhltn3@fpt.com.vn>
2021-11-23 15:13:58 +0700

    remove zipkin from staging

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'tracing' => \App\Http\Middleware\ZipkinTracing::class,
+        // 'tracing' => \App\Http\Middleware\ZipkinTracing::class,
     ];
 }
