f13291d057c722ab87cabade605e39af6253e056
DESKTOP-MOCPJLG\khoatb2 <khoatb2@fpt.com.vn>
2021-11-26 14:26:23 +0700

    chart doanh thu bao hiem hdi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Services\ChartService;
+use Illuminate\Support\Facades\Redis;
+
 class HomeController extends MY_Controller
 {
     /**
@@ -21,5 +25,25 @@ class HomeController extends MY_Controller
     public function index()
     {
         return view('home');
+    }
+    public function getDataChart()
+    {
+
+        $charService = new ChartService();
+        $keyName = config('constants.REDIS_KEY.CHART_DOANH_THU_BAO_HIEM_HDI');
+        if (Redis::exists($keyName)) {
+            $data = unserialize(Redis::get($keyName));
+            if (!array_search(date('d-m-Y',strtotime("-1 days")), array_column($data, 'date'))) {
+                $data_prev_day = $charService->getDataChartADayAgo();
+                if (!empty($data_prev_day)) {
+                    $data[] = $data_prev_day[0];
+                    Redis::set($keyName, serialize($data));
+                }
+            }
+        } else {
+            $data = $charService->getDataChart30DaysAgo();
+            Redis::set($keyName, serialize($data));
+        }
+        return $data;
     }
 }
