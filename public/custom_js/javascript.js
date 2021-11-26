aa5eb6be08df3f39f0b3ba73a523ce9c74fd1131
DESKTOP-MOCPJLG\khoatb2 <khoatb2@fpt.com.vn>
2021-11-26 14:53:15 +0700

    sidebar sortable

--- public/custom_js/javascript.js
+++ public/custom_js/javascript.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    $('#sidebar').sortable({
+        axis: "y",
+    });
     $(document).pjax('a', '#pjax');
     $('aside li.nav-item a').on('click', function (e) {
         if ($(this).attr('href') != '#') {
