0c07fdcc59f0a4f6988539ce452dee40ef3cfb84
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2021-11-28 18:42:14 +0700

    update key redis

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@ $listRedisKey = [
     'MODULE_BY_ROLE_ID' => 'acl_role_module', // acl_role_moduleX , X là role_id
     'SETTINGS' => 'settings',
     'LIST_CHECKLIST_ID' =>'list_check_list_id',
-    'CHART_DOANH_THU_BAO_HIEM_HDI' => 'chart_doanh_thu_bao_hiem_hdi'
+    'CHART_DOANH_THU_BAO_HIEM_HDI' => 'chart_doanh_thu_bao_hiem_hdi_key'
 ];
 return [
     'ADMIN' => 1, // role_id of admin
