add2f0d773c6994797e6e387170e47de5c6ed723
DESKTOP-MOCPJLG\khoatb2 <khoatb2@fpt.com.vn>
2021-12-08 05:21:36 -0500

    update close request

--- app/Http/Controllers/Hdi/ClosehelprequestController.php
+++ app/Http/Controllers/Hdi/ClosehelprequestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Hdi;
 use App\Http\Controllers\MY_Controller;
 use App\Services\ContractService;
 use App\Services\HelpRequestService;
+use GrahamCampbell\ResultType\Result;
 use Illuminate\Http\Request;;
 
 class ClosehelprequestController extends MY_Controller
@@ -25,21 +26,26 @@ class ClosehelprequestController extends MY_Controller
         $request->validate([
             'contractNo' =>'required'
         ]);
-        
+        $this->addToLog($request);
+        $result = [];
         $contractService = new ContractService();
         $helpReqeustService = new HelpRequestService();
         $contract_info_response = $contractService->getContractInfo($request); // call api get contract info
         if(empty($contract_info_response->data)){
-            return redirect()->back()->withErrors(['error'=>"Hợp đồng không tồn tại!"]);
+            $result['error'] = "Hợp đồng không tồn tại!";
+        }else{
+            $contract_info = $contract_info_response->data[0];
+            $list_report_response = $helpReqeustService->getListReportByContract($contract_info); // call api get list report by contract
+            if(empty($list_report_response->data)){
+                $result['error'] = "Không có yêu cầu hỗ trợ nào!";
+            }else{
+                // continue
+                $result['data'] = $list_report_response->data;
+                $result['contract'] = $request->contractNo;
+            }
         }
-        $contract_info = $contract_info_response->data[0];
-        $list_report_response = $helpReqeustService->getListReportByContract($contract_info); // call api get list report by contract
-        if(empty($list_report_response->data)){
-            return redirect()->back()->withErrors(['error'=>"Không có yêu cầu hỗ trợ nào!"]);
-        }
-        // continue
-        $this->addToLog($request);
-        return view('helprequest.list_request')->with(['listReport'=>$list_report_response->data]);
+        return $result;
+        // return view('helprequest.index')->with(['listReport'=>$list_report_response->data]);
     }
 
     public function closeRequest(Request $request)
--- public/custom_js/closerequest.js
+++ public/custom_js/closerequest.js
@@ -8,7 +8,56 @@ function closeRequest(_this) {
     };
     callAPIHelper("/closehelprequest/closeRequest", param, 'POST', successCloseRequest,li_tag);
 }
+function getListReport(_this){
+    let form = $(_this).closest('form');
+    param = {
+        _token:$('meta[name="csrf-token"]').attr('content'),
+        contractNo:$(form).find('input[name="contractNo"]').val()
+    };
+    callAPIHelper("/closehelprequest/getListReportByContract",param,'POST',successCallGetListReport);
+}
+function successCallGetListReport(reponse){
+    if(reponse.error != undefined){
+        showListReport.innerHTML = '';
+        showError(reponse.error);
+    }else{
+        var listColor = ['warning','info','primary','sucess'];
+        var html = `<div> Contract : `+reponse.contract+`</div>`;
+        for (const [key, report] of Object.entries(reponse.data) ){
+            html += `<li class="position-relative booking" id ="`+report.id+`">
+            <div class="media">
+                <div class="media-body">
+                    <h5 class="mb-4">ID: `+report.reportId;
+                    for (const [key2, step] of Object.entries(report.stepStatus)) {
+                        html += `<span class="badge badge-`+listColor[key2]+` ml-3">`+step.name+`</span>`
+                    };
+                    html+= `</h5>
+                    <div class="mb-3">
+                        <span class="mr-2 d-block d-sm-inline-block mb-2 mb-sm-0">Report Type:</span>
+                        <span class="bg-light-green">`+report.reportType+` -`+report.reportName+`</span>
+                    </div>
+                    <div class="mb-3">
+                        <span class="mr-2 d-block d-sm-inline-block mb-2 mb-sm-0">Sub Type name:</span>
+                        <span class="bg-light-green">`+report.subTypeName+`</span>
+                    </div>
 
+                    <div class="mb-5">
+                        <span class="mr-2 d-block d-sm-inline-block mb-1 mb-sm-0">Note:</span>
+                        <span class="pr-2 mr-2">`+report.note+`</span>
+                    </div>
+                </div>
+            </div>`;
+            if(report.reportType === 'HT-KYTHUAT' && report.isShowBtnCancel == 1){
+            html+=`
+            <div class="buttons-to-right">
+                <a onclick="dialogConfirmWithAjax(closeRequest,this)" type="button"class="btn-red mr-2"><i class="far fa-times-circle mr-2"></i>Close</a>
+            </div>`;
+            }
+        html+=`</li>`;
+        };
+        showListReport.innerHTML = html;
+    }
+}
 function successCloseRequest(response,li_tag) {
     if (response == true) {
         $(li_tag).remove();
