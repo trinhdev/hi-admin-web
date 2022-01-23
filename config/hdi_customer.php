951b994d03b0f8204b4a9e9a6a4cf17552d34c86
Le Thi Ngoc Oanh (FTEL HiFPT) <oanhltn3@fpt.com.vn>
2022-01-23 19:49:57 +0700

    Update hdi_customer.php revert

--- config/hdi_customer.php
+++ config/hdi_customer.php
@@ -14,7 +14,7 @@ return [
         'HI_CUSTOMER_SECRET'    => 'xxxxxxhifpt2018'
     ],
     'production'                => [
-        'URI_BASE_HI_AUTH'      => 'http://hi-authapi.fpt.vn/',
+        'URI_BASE_HI_AUTH'      => 'hi-authapi.fpt.vn/',
         'HI_AUTH_VERSION'       => 'v1',
         'HI_CUSTOMER_CLIENT_ID' => 'hifpt_customer_local',
         'HI_CUSTOMER_SECRET'    => 'xxxxxxhifpt2018'
