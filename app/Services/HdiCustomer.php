dc2f4e41c9bdfafca8754aa922be32fa974d4af1
unknown <oanhltn3@fpt.com.vn>
2022-01-25 13:30:11 +0700

    test otp

--- app/Services/HdiCustomer.php
+++ app/Services/HdiCustomer.php
@@ -69,6 +69,7 @@ class HdiCustomer
 
         $time = microtime(true);
         $output = curl_exec($ch);
+        my_debug($url.'</br>'.$output);
         $timeRun = microtime(true) - $time;
         // if (curl_errno($ch)) {
             // my_debug($url.'</br>'.curl_error($ch));
