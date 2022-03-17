42ed14a41880d9c52c406a7f0802dd4301dd8257
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2022-03-17 12:07:47 +0700

    update css + policy

--- resources/views/popup/_action-menu.blade.php
+++ resources/views/popup/_action-menu.blade.php
@@ -1,6 +1,6 @@
-@if(Auth::user()->role_id == ADMIN || $aclCurrentModule->update == 1)
-<div class="d-flex col-md-12">
-    <a style="float: left; margin-right: 5px" type="button" href="{{ route('popupmanage.view', [$model->id ]) }}" class="btn btn-sm fas fa-eye btn-icon bg-primary col-md-6"></a>
-    <a style="" type="button" href="{{ route('popupmanage.edit', [$model->id]) }}" class="btn btn-sm fas fa-edit btn-icon bg-olive col-md-6"></a>
+<div class="d-flex justify-content-center">
+    <a style="float: left; margin-right: 5px" type="button" href="{{ route('popupmanage.view', [$model->id ]) }}" class="btn btn-sm fas fa-eye btn-icon bg-primary"></a>
+    @if(Auth::user()->role_id == ADMIN || $aclCurrentModule->update == 1)
+    <a style="" type="button" href="{{ route('popupmanage.edit', [$model->id]) }}" class="btn btn-sm fas fa-edit btn-icon bg-olive"></a>
+    @endif
 </div>
-@endif
