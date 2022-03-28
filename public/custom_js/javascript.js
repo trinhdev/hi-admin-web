2963991a0a82c42e2df34a29e80e4556105280de
unknown <oanhltn3@fpt.com.vn>
2022-03-28 10:22:18 +0700

    fix template staging

--- public/custom_js/javascript.js
+++ public/custom_js/javascript.js
@@ -2,55 +2,25 @@ $(document).ready(function () {
     // $('#sidebar').sortable({
     //     axis: "y",
     // });
-    if (window.history && window.history.pushState) {
+    // $(document).pjax('a', '#pjax');
+    // $('aside li.nav-item a').on('click', function (e) {
+    //     if ($(this).attr('href') != '#') {
+    //         $('aside').find(".menu-open > .nav-treeview").not($(this).parents('.menu-open > .nav-treeview')).slideUp()
+    //         $('aside').find(".menu-open").not($(this).parents('.menu-open')).removeClass("menu-is-opening menu-open");
+    //         $('li a').removeClass("active");
+    //         $(this).addClass("active");
+    //     }
+    //     $(this).parents('.nav-treeview').prevAll('.nav-link').addClass('active');
 
-        // window.history.pushState('forward', null, './#forward');
-        if ($('#spinner').length) {
-            console.log('ready');
-            $('#spinner').removeClass('show');
-            $('#spinner').addClass('hide');
-        }
-        // $(window).on('popstate', function() {
-        //     if($('#spinner').length) {
-        //         console.log('ready');
-        //         $('#spinner').removeClass('show');
-        //         $('#spinner').addClass('hide');
-        //     }
-        // });
-
+    // });
+    var moduleActive = document.querySelector('aside .active');
+    var parentModuleActive = moduleActive.parentNode.parentNode.parentNode;
+    if(parentModuleActive.classList.contains('menu')){ // if parent module is menu
+        parentModuleActive.classList.add('menu-open');
+        parentModuleActive.querySelector('.nav-link').classList.add('active');
     }
-
-
-    $(document).pjax('a', '#pjax');
-        $('aside li.nav-item a').on('click', function (e) {
-            if ($(this).attr('href') != '#') {
-                $('aside').find(".menu-open > .nav-treeview").not($(this).parents('.menu-open > .nav-treeview')).slideUp()
-                $('aside').find(".menu-open").not($(this).parents('.menu-open')).removeClass("menu-is-opening menu-open");
-                $('li a').removeClass("active");
-                $(this).addClass("active");
-            }
-            $(this).parents('.nav-treeview').prevAll('.nav-link').addClass('active');
-
-        // $(document).pjax('a', '#pjax');
-        // $('aside li.nav-item a').on('click', function (e) {
-        //     if ($(this).attr('href') != '#') {
-        //         $('aside').find(".menu-open > .nav-treeview").not($(this).parents('.menu-open > .nav-treeview')).slideUp()
-        //         $('aside').find(".menu-open").not($(this).parents('.menu-open')).removeClass("menu-is-opening menu-open");
-        //         $('li a').removeClass("active");
-        //         $(this).addClass("active");
-        //     }
-        //     $(this).parents('.nav-treeview').prevAll('.nav-link').addClass('active');
-
-        // });
-        var moduleActive = document.querySelector('aside .active');
-        var parentModuleActive = moduleActive.parentNode.parentNode.parentNode;
-        if(parentModuleActive.classList.contains('menu')){ // if parent module is menu
-            parentModuleActive.classList.add('menu-open');
-            parentModuleActive.querySelector('.nav-link').classList.add('active');
-        }
-        // document.querySelector('.active').closest('.nav-item menu > li').classList.add('is-active');
-        // reloadPjax();
-    });
+    // document.querySelector('.active').closest('.nav-item menu > li').classList.add('is-active');
+    // reloadPjax();
 });
 
 function reloadPjax() {
@@ -64,7 +34,7 @@ function reloadPjax() {
 
 function handleSubmit(e, form, withPopup = true) {
     e.preventDefault();
-    if (withPopup) {
+    if(withPopup){
         Swal.fire({
             title: 'Are you sure?',
             text: "Please Confirm This Action",
@@ -74,39 +44,28 @@ function handleSubmit(e, form, withPopup = true) {
             confirmButtonColor: '#3085d6',
             confirmButtonText: 'Yes, Confirmed!',
             reverseButtons: true
-
+    
         }).then((result) => {
             if (result.isConfirmed) {
                 form.submit();
-                let submitBtn = $(form).closest('form').find('button').append('&ensp;<i class="fa fa-spinner fa-spin"></i>').prop('disabled', true);
-                $('form').find(':button').prop('disabled', true);
-                $("#spinner").toggle("show");
                 showLoading();
             }
         });
-    } else {
+    }else{
         form.submit();
-        let submitBtn = $(form).closest('form').find('button').append('&ensp;<i class="fa fa-spinner fa-spin"></i>').prop('disabled', true);
-        $('form').find(':button').prop('disabled', true);
-        $("#spinner").toggle("show");
-    }
-
-    if (e.result == true) {
-        // $("#spinner").addClass("hide");
-        console.log('end submit');
-        showLoading(form);
+        showLoading();
     }
 
 }
-function showLoading(form = null) {
+function showLoading(){
     $("#spinner").addClass("show");
     $(form).closest('form').find('button').append('&ensp;<i class="fa fa-spinner fa-spin"></i>').prop('disabled', true);
     $('form').find(':button').prop('disabled', true);
-    setTimeout(function () {
+    setTimeout(function() {
         $("#spinner").removeClass("show");
         $(form).closest('form').find('button').append('&ensp;<i class="fa fa-spinner fa-spin"></i>').prop('disabled', false);
         $('form').find(':button').prop('disabled', false);
-    }, 5000);
+      },5000);
 }
 function dialogConfirmWithAjax(sureCallbackFunction, data) {
     Swal.fire({
@@ -151,7 +110,7 @@ function uploadFileExternal(file, callBack, passingData) {
         }
     });
     var formData = new FormData();
-    formData.append("file", file, file.name);
+    formData.append("file", file,file.name);
     $.ajax({
         type: 'POST',
         url: '/file/uploadImageExternal',
@@ -160,7 +119,7 @@ function uploadFileExternal(file, callBack, passingData) {
         contentType: false,
         processData: false,
         success: (data) => {
-            callBack(data, passingData);
+            callBack(data,passingData);
         },
         error: function (xhr) {
             var errorString = '';
@@ -206,54 +165,10 @@ function getDataInForm(form) {
     return data;
 }
 
-function findElementInArrayObjectByKeyValue(array, key, value) {
+function findElementInArrayObjectByKeyValue(array ,key, value){
     return array.find(object => object[key] == value);
 }
 
 function isEmpty(str) {
-    return (!str || str.length === 0);
-}
-
-function removeVietnameseTones(str) {
-    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
-    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
-    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
-    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
-    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
-    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
-    str = str.replace(/đ/g, "d");
-    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
-    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
-    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
-    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
-    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
-    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
-    str = str.replace(/Đ/g, "D");
-    // Some system encode vietnamese combining accent as individual utf-8 characters
-    // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
-    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
-    str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
-    // Remove extra spaces
-    // Bỏ các khoảng trắng liền nhau
-    str = str.replace(/ + /g, " ");
-    str = str.trim();
-    // Remove punctuations
-    // Bỏ dấu câu, kí tự đặc biệt
-    str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, " ");
-    return str;
-}
-
-function serializeObject(obj) {
-    var jsn = {};
-    $.each(obj, function () {
-        if (jsn[this.name]) {
-            if (!jsn[this.name].push) {
-                jsn[this.name] = [jsn[this.name]];
-            }
-            jsn[this.name].push(this.value || '');
-        } else {
-            jsn[this.name] = this.value || '';
-        }
-    });
-    return jsn;
+    return (!str || str.length === 0 );
 }
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -34,12 +34,12 @@
     <link rel="stylesheet" href="{{url(Theme::find(config('platform_config.current_theme'))->themesPath)}}/plugins/bootstrap-datetimepicker/css/tempus-dominus.min.css">
     <link rel="stylesheet" href="{{url(Theme::find(config('platform_config.current_theme'))->themesPath)}}/plugins/datatables-searchpanes/css/searchPanes.bootstrap4.min.css">
     {{-- <link rel="stylesheet" href="{{url(Theme::find(config('platform_config.current_theme'))->themesPath)}}/plugins/app-assets/css/colors.min.css"> --}}
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+    {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css"> --}}
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css">
     {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.bootstrap4.min.css"/> --}}
     
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
-    <link rel="stylesheet" href="{{url(Theme::find(config('platform_config.current_theme'))->themesPath)}}/plugins/datatables/css/jquery.dataTables.min.css">
+    {{-- <link rel="stylesheet" href="{{url(Theme::find(config('platform_config.current_theme'))->themesPath)}}/plugins/datatables/css/jquery.dataTables.min.css"> --}}
 
     <!-- Datatables -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
