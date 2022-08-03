aa3ebbce9f7a5139c4dcd7d3d2d7191e09496c36
trinhhdp <trinhhdp@fpt.com.vn>
2022-08-03 15:49:23 +0700

    update ftel phone check target visibles

--- public/custom_js/ftel_phone.js
+++ public/custom_js/ftel_phone.js
@@ -76,7 +76,7 @@ function datatableFtelPhoneExport(){
         ],
         "columnDefs": [
             {
-                "targets": [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+                "targets": [9,10,11,12,13,14,15,16,17,18,19,20,21,22],
                 "visible": false
             }
         ]
