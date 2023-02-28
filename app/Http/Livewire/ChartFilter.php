8d97c0e430774c8d7d7feb2fee6308f3fdc033d7
trinhhdp <trinhhdp@fpt.com.vn>
2023-02-28 14:02:14 +0700

    update datatables

--- app/Http/Livewire/ChartFilter.php
+++ app/Http/Livewire/ChartFilter.php
@@ -17,14 +17,14 @@ class ChartFilter extends Component
 
     public function dateSelected(string $chart, string $date, string $typ, $limit, $duration)
     {
-        if (split_date($date)) {
-            $from = \Carbon\Carbon::parse(split_date($date)[0])->format('Y-m-d');
-            $to = \Carbon\Carbon::parse(split_date($date)[1])->format('Y-m-d');
-        }
-        $service = new TrackingService();
-        $limit      = (int) $limit;
-        $duration   = (int) $duration;
         try {
+            if (split_date($date)) {
+                $from = \Carbon\Carbon::parse(split_date($date)[0])->format('Y-m-d');
+                $to = \Carbon\Carbon::parse(split_date($date)[1])->format('Y-m-d');
+            }
+            $service = new TrackingService();
+            $limit      = (int) $limit;
+            $duration   = (int) $duration;
             $data = $service->get_active_customers($chart,$from ?? '', $to ?? '', $limit??10, $duration??0);
             $datasets = collect($data->detail)['values'];
             foreach ($datasets as $key => $value) {
